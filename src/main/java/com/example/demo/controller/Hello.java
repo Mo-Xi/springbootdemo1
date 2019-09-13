ebee108ebadd65449aaf4d4d67d3e2233dee43ba
Mo-Xi <hzk572627905@gmail.com>
2019-09-13 15:38:08 +0800

    add bootstrap

--- src/main/java/com/example/demo/controller/Hello.java
+++ src/main/java/com/example/demo/controller/Hello.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class Hello {
 
-    @GetMapping("/hello")
+    @GetMapping("/index")
     public String hello(@RequestParam(name = "name",required = false,defaultValue = "sss") String name, Model model){
         model.addAttribute("name",name);
-        return "hello";
+        return "index";
     }
 
 }
